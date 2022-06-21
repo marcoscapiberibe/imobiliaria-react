9c1ae3d59f346b772d6f846241b54419c5b98a4c
Marcos Capiberibe <marcoscapiberibe@hotmail.com>
2022-06-21 03:02:54 -0300

    Criação do componente Header e da Navbar
    Criação do componente Header e da Navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
 import {Jumbotron, Button} from 'react-bootstrap';
+import Header from './components/Header'
+// import OffcanvasExample from './components/Header';
 
 function App() {
   return (
-    <div className="App">
+    <>
+    <Header />
+    <div className="App">      
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <Button variant="info">Teste</Button>{'Legenda'}
       </header>
     </div>
+    </>
   );
 }
 
