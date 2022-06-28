fcfaba3b52f03bb4fc97889cfc4b2a6e20b643db
Marcos Capiberibe <marcoscapiberibe@hotmail.com>
2022-06-28 16:16:42 -0300

    Criação do Carrosel
    Criação do Carrosel

--- src/App.js
+++ src/App.js
@@ -4,20 +4,24 @@ import 'bootstrap/dist/css/bootstrap.css'
 import { Button } from 'react-bootstrap';
 import Header from './components/Header'
 import Item from './components/Item'
+import Carrosel from './components/Carrosel'
 // import OffcanvasExample from './components/Header';
 
 function App() {
   return (
     <>
-    <header>
-    <Header />
-    </header>
-    <main>
-    <section className="barra--pesquisa">  
-    <Item />
-    </section>
-    </main>
-    {/* <div className="App">      
+      <header>
+        <Header />
+      </header>
+      <main>
+        <section>
+          <Carrosel />
+        </section>
+        <section className="barra--pesquisa">
+          <Item />
+        </section>
+      </main>
+      {/* <div className="App">      
       <header className="App-header">
         <Button variant="info">Teste</Button>{'Legenda'}
       </header>
