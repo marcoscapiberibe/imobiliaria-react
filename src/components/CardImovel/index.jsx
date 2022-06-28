0c68e85ebd0b7c8c8234abfe5e2771ca35b141a1
Marcos Capiberibe <marcoscapiberibe@hotmail.com>
2022-06-28 20:35:01 -0300

    Edição do Card 01 e inclusão de foto
    Edição do Card 01 e inclusão de foto

--- src/components/CardImovel/index.jsx
+++ src/components/CardImovel/index.jsx
@@ -1,20 +1,20 @@
 import '../CardImovel/CardImovel.css';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import CardImovel01 from '../CardImovel/Card01.jpg';
 
 
 
 function CardImovel() {
   return (
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Img variant="top" src={CardImovel01} />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>Apartamento</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        Apto. TÉRREO c/ 68m² de área privativa, sala p/ 2 ambientes c/ armário PROJETADO e varanda, 3 quartos c/ armários PROJETADOS s/ 1 suíte
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="secondary">Quero este imóvel</Button>
       </Card.Body>
     </Card>
   );
