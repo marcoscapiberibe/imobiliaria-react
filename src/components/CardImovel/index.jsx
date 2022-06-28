1e4ae02774439efc628a8525533232310ed5e6e1
Marcos Capiberibe <marcoscapiberibe@hotmail.com>
2022-06-28 20:49:34 -0300

    Inclusão de 2 Cards
    Inclusão de 2 Cards

--- src/components/CardImovel/index.jsx
+++ src/components/CardImovel/index.jsx
@@ -2,6 +2,8 @@ import '../CardImovel/CardImovel.css';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import CardImovel01 from '../CardImovel/Card01.jpg';
+import CardImovel02 from '../CardImovel/Card02.jpg';
+import CardImovel03 from '../CardImovel/Card03.jpg';
 
 
 
@@ -16,6 +18,25 @@ function CardImovel() {
         </Card.Text>
         <Button variant="secondary">Quero este imóvel</Button>
       </Card.Body>
+
+      <Card.Img variant="top" src={CardImovel02} />
+      <Card.Body>
+        <Card.Title>Apartamento</Card.Title>
+        <Card.Text>
+        Apartamento com 344 metros quadrados com 4 quartos sendo 4 suites e 2 banheiros. Possui academia, espaço gourmet, área verde de jardim, área de recreação infantil, espaço para festas.
+        </Card.Text>
+        <Button variant="secondary">Quero este imóvel</Button>
+      </Card.Body>
+
+      <Card.Img variant="top" src={CardImovel03} />
+      <Card.Body>
+        <Card.Title>Apartamento</Card.Title>
+        <Card.Text>
+        Apartamento com 164 metros quadrados, 3 quartos sendo 3 suites e 5 banheiros. Possui academia, área verde de jardim, parquinho com diferentes brinquedos, salão de festas.
+        </Card.Text>
+        <Button variant="secondary">Quero este imóvel</Button>
+      </Card.Body>
+      
     </Card>
   );
 }
