814ba06f22aee0578fe1effe7580b8894f4036e6
Marcos Capiberibe <marcoscapiberibe@hotmail.com>
2022-06-28 23:45:47 -0300

    correção de alinhamento dos cards
    correção de alinhamento dos cards

--- src/components/CardImovel/index.jsx
+++ src/components/CardImovel/index.jsx
@@ -12,11 +12,11 @@ import CardImovel03 from '../CardImovel/Card03.jpg';
 
 function CardImovel() {
     return (
-        <Container>
-        <Row xs={1} md={3} className="g-4 justify-content-center align-items-center">
+        <Container className='card--row'>
+        <Row xs={1} md={3} className="g-4 justify-content-center align-items-center card--row">
 
             <Col>
-                <Card style={{ width: '18rem' }}>
+                <Card style={{ width: '18rem'}} className="card--estilo">
                     <Card.Img variant="top" src={CardImovel01} />
                     <Card.Body>
                         <Card.Title>Apartamento</Card.Title>
@@ -29,7 +29,7 @@ function CardImovel() {
             </Col>
 
             <Col>
-                <Card style={{ width: '18rem' }}>
+                <Card style={{ width: '18rem' }} className="card--estilo">
                     <Card.Img variant="top" src={CardImovel02} />
                     <Card.Body>
                         <Card.Title>Apartamento</Card.Title>
@@ -42,7 +42,46 @@ function CardImovel() {
             </Col>
 
             <Col>
-                <Card style={{ width: '18rem' }}>
+                <Card style={{ width: '18rem' }}  className="card--estilo">
+                    <Card.Img variant="top" src={CardImovel03} />
+                    <Card.Body>
+                        <Card.Title>Apartamento</Card.Title>
+                        <Card.Text>
+                            Apartamento com 164 metros quadrados, 3 quartos sendo 3 suites e 5 banheiros. Possui academia, área verde de jardim, parquinho com diferentes brinquedos, salão de festas.
+                        </Card.Text>
+                        <Button variant="secondary">Quero este imóvel</Button>
+                    </Card.Body>
+                </Card>
+            </Col>
+
+            <Col>
+                <Card style={{ width: '18rem' }} className="card--estilo">
+                    <Card.Img variant="top" src={CardImovel01} />
+                    <Card.Body>
+                        <Card.Title>Apartamento</Card.Title>
+                        <Card.Text>
+                            Apto. TÉRREO c/ 68m² de área privativa, sala p/ 2 ambientes c/ armário PROJETADO e varanda, 3 quartos c/ armários PROJETADOS s/ 1 suíte
+                        </Card.Text>
+                        <Button variant="secondary">Quero este imóvel</Button>
+                    </Card.Body>
+                </Card>
+            </Col>
+
+            <Col>
+                <Card style={{ width: '18rem' }} className="card--estilo">
+                    <Card.Img variant="top" src={CardImovel02} />
+                    <Card.Body>
+                        <Card.Title>Apartamento</Card.Title>
+                        <Card.Text>
+                            Apartamento com 344 metros quadrados com 4 quartos sendo 4 suites e 2 banheiros. Possui academia, espaço gourmet, área verde de jardim, área de recreação infantil, espaço para festas.
+                        </Card.Text>
+                        <Button variant="secondary">Quero este imóvel</Button>
+                    </Card.Body>
+                </Card>
+            </Col>
+
+            <Col>
+                <Card style={{ width: '18rem' }} className="card--estilo">
                     <Card.Img variant="top" src={CardImovel03} />
                     <Card.Body>
                         <Card.Title>Apartamento</Card.Title>
